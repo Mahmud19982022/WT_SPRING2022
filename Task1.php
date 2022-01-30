f8daa12b7b5bb1e3303924175bb0f9051890a38d
Mahmud19982022 <abdullahalmahmud06337@gmail.com>
2022-01-30 11:38:08 +0600

    first commmit

---
+++ Task1.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+</head>
+<body>
+<h1>REGISTRATION FORM</h1>
+
+<form action="/action_page.php">
+    
+    First Name:
+    <td><input type="text" name="fname"> <br>
+   <br> Last Name:
+    <input type="text" name="lname"><br>
+     
+    <br> Age :
+                <input type="text" name="age"><br>
+   
+          <br> Designation:
+          <input type="radio" name="level"> Junior Programmer
+          <input type="radio" name="level" >Seniorr Programmer
+          <input type="radio" name="level" >Project Lead
+
+          <br> Prefeered Language:
+          <input type="radio" name="level"> JAVA
+          <input type="radio" name="level" >PHP
+          <input type="radio" name="level" >C++
+         
+          <br> Email :
+          <input type="text" name="age"><br>
+          <br> Password :
+          <input type="password" name=""><br>
+
+          <br> 
+         <label for="myfile">Please Chose A File:</label>
+  <input type="file" id="myfile" name="myfile"><br>
+          <br> 
+          <input type="submit" >
+          
+          <input type="reset">
+
+    
+
+</body
+</html>
+
+
+    
+
+
+
+
