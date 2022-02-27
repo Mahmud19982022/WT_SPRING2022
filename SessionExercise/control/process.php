db7f1226d0620a245968c0ae5dc46f3a239bae07
Mahmud19982022 <98643653+Mahmud19982022@users.noreply.github.com>
2022-02-27 10:34:25 +0600

    Add files via upload

---
+++ SessionExercise/control/process.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if(isset($_SESSION["name"]))
+{
+    header("location: profile.php");
+
+if(isset($_POST["Submit"]))
+{
+    if($_POST["name"] == "abc" && $_POST["pwd"] == "123"){
+        $_SESSION["name"] = $_POST["name"];
+        $_SESSION["pass"] = $_POST["pwd"];
+        header("location: ../view/profile.php");
+    }
+    else{
+        echo"Incorrect username or password.<br>";
+    }
+}
+}
+?>
